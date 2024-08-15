0e4f953d9bb49ed0c67886fde375d8b9515907aa
Srijan Ojha <106486293+srijan-99@users.noreply.github.com>
2024-08-15 19:57:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,13 @@ const app = express();
 const PORT = 5000;
 
 app.use(cors());
+app.use(cors(
+    {
+        origin:["https://backend-lepide-assignment.vercel.app/"],
+        methods: ["POST", "GET", "PUT", "DELETE"],
+        credentials: true
+    }
+));
 app.use(express.json());
 
 const storage = multer.diskStorage({
