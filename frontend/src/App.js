64582dbc281433455f93f68ea81cf3865211a30b
Srijan Ojha <106486293+srijan-99@users.noreply.github.com>
2024-08-15 19:29:29 +0530

    upload frontend react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Upload from './components/upload/Upload.jsx';
+
+const App = () => {
+  return (
+    <div className="App" style={{ padding: "10px" }}>
+      <h1>File Upload System</h1>
+      <Upload />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/upload/Upload.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Upload = () => {
+    const [file, setFile] = useState(null);
+    const [content, setContent] = useState('');
+
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+    };
+
+    const handleUpload = async () => {
+        const formData = new FormData();
+        formData.append('file', file);
+
+        try {
+            const response = await axios.post('http://localhost:5000/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            setContent(response.data.content);
+        } catch (error) {
+            console.error('Error uploading file:', error);
+        }
+    };
+
+    return (
+        <div>
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={handleUpload}>Upload</button>
+            <div className="displayContent"
+                style={{
+                    textWrap: "wrap",
+                    border: "1px solid black",
+                    margin: "10px 0",
+                    padding: "5px 10px"
+                }}
+            >
+                <pre>{content}</pre>
+            </div>
+        </div>
+    );
+};
+
+export default Upload;
