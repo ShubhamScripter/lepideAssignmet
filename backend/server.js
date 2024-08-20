43b6e03f8686fcc31d313f65f58cda92a32508d7
unknown <srijanojha26@gmail.com>
2024-08-20 11:33:49 +0530

    add @xenova/transformers llm summarization endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,53 @@
-const express = require('express');
-const multer = require('multer');
-const cors = require('cors');
-const fs = require('fs');
-const path = require('path');
+import express, { json } from "express";
+import multer, { diskStorage } from "multer";
+import cors from "cors";
+import { readFileSync } from "fs";
+import path, { join } from "path";
+import { fileURLToPath } from "url";
+import { pipeline } from '@xenova/transformers';
 
 const app = express();
 const PORT = 5000;
 
-app.use(cors());
-app.use(cors(
-    {
-        origin:["https://backend-lepide-assignment.vercel.app/"],
-        methods: ["POST", "GET", "PUT", "DELETE"],
-        credentials: true
-    }
-));
-app.use(express.json());
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
-    },
+app.use(cors());
+app.use(
+  cors({
+    origin: ["https://backend-lepide-assignment.vercel.app/"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(json());
+
+const storage = diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
 const upload = multer({ storage });
+const pipe = await pipeline("summarization");
 
-app.post('/upload', upload.single('file'), (req, res) => {
-    if (!req.file) {
-        return res.status(400).send('No file uploaded.');
-    }
-    const filePath = path.join(__dirname, 'uploads', req.file.filename);
-    const fileContent = fs.readFileSync(filePath, 'utf-8');
-    res.json({ content: fileContent });
+app.post("/upload", upload.single("file"), async (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
+  }
+  const filePath = join(__dirname, "uploads", req.file.filename);
+  const fileContent = readFileSync(filePath, "utf-8");
+
+  // summarize
+  const result = await pipe(fileContent, { max_length: 30 });
+  console.log({ content: result });
+
+  res.status(200).json({ result });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
--- frontend/src/components/upload/Upload.jsx
+++ frontend/src/components/upload/Upload.jsx
@@ -1,46 +1,50 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const Upload = () => {
-    const [file, setFile] = useState(null);
-    const [content, setContent] = useState('');
+  const [file, setFile] = useState(null);
+  const [content, setContent] = useState("");
 
-    const handleFileChange = (e) => {
-        setFile(e.target.files[0]);
-    };
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
 
-    const handleUpload = async () => {
-        const formData = new FormData();
-        formData.append('file', file);
+  const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append("file", file);
 
-        try {
-            const response = await axios.post('https://lepide-assignment.onrender.com/upload', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
-            setContent(response.data.content);
-        } catch (error) {
-            console.error('Error uploading file:', error);
-        }
-    };
+    try {
+      const apiEndpoint = "https://lepide-assignment.onrender.com/upload";
+      // const apiEndpoint = "http://localhost:5000/upload";
+      const response = await fetch(apiEndpoint, {
+        method: "POST",
+        body: formData,
+      });
+      // setContent(response.data.content);
+      const data = await response.json();
+      setContent(data.result[0].summary_text);
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
+  };
 
-    return (
-        <div>
-            <input type="file" onChange={handleFileChange} />
-            <button onClick={handleUpload}>Upload</button>
-            <div className="displayContent"
-                style={{
-                    textWrap: "wrap",
-                    border: "1px solid black",
-                    margin: "10px 0",
-                    padding: "5px 10px"
-                }}
-            >
-                <pre>{content}</pre>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
+      <div
+        className="displayContent"
+        style={{
+          textWrap: "wrap",
+          border: "1px solid black",
+          margin: "10px 0",
+          padding: "5px 10px",
+        }}
+      >
+        <pre>{content}</pre>
+      </div>
+    </div>
+  );
 };
 
 export default Upload;
